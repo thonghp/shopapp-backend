8049f22a8a3a35d2280638130f8f41e436f70452
thonghp <thonghoangpham123@gmail.com>
2024-05-09 01:59:34 +0700

    code order controller

---
+++ src/main/java/com/example/shopapp/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.example.shopapp.controllers;
+
+import com.example.shopapp.dtos.UserDTO;
+import com.example.shopapp.dtos.UserLoginDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDTO userDTO, BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .collect(Collectors.toList());
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            if (!userDTO.getPassword().equals(userDTO.getRetypePassword())) {
+                return ResponseEntity.badRequest().body("Password does not match");
+            }
+            return ResponseEntity.ok("Register successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        // Kiểm tra thông tin đăng nhập và sinh token
+        // Trả về token trong response
+        return ResponseEntity.ok("some token");
+    }
+}
